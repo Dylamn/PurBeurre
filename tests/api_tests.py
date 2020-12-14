db462f701633bef72b295878edf37d4da0bfa528
Dylan <dylan.vallee@hotmail.com>
2020-12-14 09:43:43 +0100

    Simple docstrings for unit tests

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -3,7 +3,11 @@ import requests
 
 
 class ApiTest(unittest.TestCase):
-    """Test case used for API endpoints."""
+    """Test case used for API endpoints.
+
+    Attributes:
+        BASE    The base URL of the API.
+    """
 
     BASE = "http://127.0.0.1:5000/"
 
@@ -15,11 +19,13 @@ class ApiTest(unittest.TestCase):
         }
 
     def test_category_store_success(self) -> None:
+        """Method which verify if a category is stored as expected."""
         response = requests.post(self.BASE + 'categories', self.request_body)
 
         self.assertEqual(201, response.status_code, response.content)
 
     def test_category_store_failure(self) -> None:
+        """Method which will verify if the validation works as expected."""
         # We remove from the request body the "name" attribute which is required.
         request_body = self.request_body.copy()
         del request_body["name"]
